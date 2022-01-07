a405f6350a98784bf57f839628946b027772ee35
Federico Gottardi <gotfede@gmail.com>
2022-01-07 17:00:08 +0100

    070122v2

--- 4yargsCLI/src/app.js
+++ 4yargsCLI/src/app.js
@@ -8,51 +8,45 @@ const {
   breakLine,
 } = require("./utils/index");
 const connection = require("./db/connection");
+const Movie = require("./models/models");
 
-const command = process.argv[2];
+const command = yargs.argv._[0]; ///
+// console.log(yargs.argv);
+
 const app = async (args) => {
-  breakLine();
   try {
     //add the movie
     if (command === "add") {
-      const movieobj = { title: args.title, actor: args.actor };
-      await connection(addMovie, movieobj);
+      const movie = await addMovie({ title: args.title, actor: args.actor });
+      console.log("New movie added: ", movie.title, movie.actor);
     }
     // read a list
     if (command === "list") {
-      await connection(listMovies);
-    }
-    // update a movie
-    if (command === "updateN") {
-      const updateObj = {
-        title: args.title,
-        updateValue: args.updateValue,
-      };
-      await connection(updateMovie, updateObj);
-    }
-    // delete movies name
-    if (command === "deleteN") {
-      const deleteObj = {
-        title: args.title,
-      };
-      await connection(deleteItemN, deleteObj);
-    }
-    // delete movies Delete actors name
-    if (command === "deleteA") {
-      const deleteObj = {
-        actor: args.actor,
-      };
-      await connection(deleteItemA, deleteObj);
+      await listMovies();
     }
 
-    if (command === "info") {
-      console.log(
-        " --add : add a list of movie \n --list : print list of movie \n --updateN : add a movie in the list \n --deleteN : delete name of the movie from the list \n --deleteA : remove actor from the list"
-      );
+    if (command === "deleteT") {
+      await deleteItemA({ title: args.title });
     }
+
+    if (command === "deleteA") {
+      await deleteItemN({ actor: args.actor });
+    }
+
+    if (command === "update") {
+      await updateMovie({
+        title: args.title,
+        updateTitle: args.updateTitle,
+      });
+    }
+    
+    // if (command === "info") {
+    //   console.log(
+    //     " --add : add a list of movie \n --list : print list of movie \n --updateN : add a movie in the list \n --deleteN : delete name of the movie from the list \n --deleteA : remove actor from the list"
+    //   );
+    // }
   } catch (error) {
     console.log(error);
   }
 };
-
 app(yargs.argv);
